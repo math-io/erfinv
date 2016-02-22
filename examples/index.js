2f1ea5333f28e91d1b4228fd9424bd196e4c2697
kgryte <kgryte@gmail.com>
2016-02-21 22:28:20 -0800

    Add example

---
+++ examples/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var linspace = require( 'compute-linspace' );
+var erfinv = require( './../lib' );
+
+var x = linspace( 0, 2, 100 );
+var y;
+var i;
+
+for ( i = 0; i < x.length; i++ ) {
+	y = erfinv( x[ i ] );
+	console.log( 'x: %d, erfinv(x): %d', x[ i ], y );
+}
