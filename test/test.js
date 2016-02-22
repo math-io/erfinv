cb8e4bd4a893b0eb1a6015ac872a40e2383102b0
kgryte <kgryte@gmail.com>
2016-02-22 01:46:01 -0800

    Add test fixtures and add tests for various input value ranges

--- test/test.js
+++ test/test.js
@@ -5,7 +5,27 @@
 var tape = require( 'tape' );
 var PINF = require( 'const-pinf-float64' );
 var NINF = require( 'const-ninf-float64' );
+var abs = require( 'math-abs' );
 var erfinv = require( './../lib' );
+
+
+// FIXTURES //
+
+var x1 = require( './fixtures/x_-0.5_0.5.json' );
+var x2 = require( './fixtures/x_-0.5_-0.75.json' );
+var x3 = require( './fixtures/x_0.5_0.75.json' );
+var x4 = require( './fixtures/x_0.75_0.9998.json' );
+var x5 = require( './fixtures/x_0.9998_0.9999..8.json' );
+var x6 = require( './fixtures/x_0.9999..8_1.json' );
+
+
+// FUNCTIONS //
+
+function almostEqual( a, b, tol ) {
+	var delta = abs( a - b );
+	tol = tol * Math.max( 1, abs( a ), abs( b ) );
+	return ( delta <= tol );
+}
 
 
 // TESTS //
@@ -70,6 +90,153 @@ tape( 'if provided a value which is either less than `-1` or greater than `1`, t
 			erfinv( value );
 		};
 	}
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[-0.5,0.5]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 4e-16;
+
+	expected = x1.expected;
+	x = x1.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[-0.5,-0.75]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 4e-16;
+
+	expected = x2.expected;
+	x = x2.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[0.5,0.75]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 4e-16;
+
+	expected = x3.expected;
+	x = x3.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[0.75,0.9998]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 2.8e-15;
+
+	expected = x4.expected;
+	x = x4.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[0.9998,0.9999..8]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 1.85e-15;
+
+	expected = x5.expected;
+	x = x5.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the inverse error function for `x` on the interval `[0.9999..8,1]', function test( t ) {
+	var expected;
+	var bool;
+	var tol;
+	var x;
+	var y;
+	var i;
+
+	tol = 1.75e-16;
+
+	expected = x6.expected;
+	x = x6.x;
+	for ( i = 0; i < x.length; i++ ) {
+		y = erfinv( x[i] );
+		if ( expected[ i ] === null ) {
+			expected[ i ] = PINF;
+		}
+		if ( y === expected[i] ) {
+			t.equal( y, expected[i], 'x: '+x[i]+', y: '+y+', expected: '+expected[i] );
+		}  else {
+			bool = almostEqual( y, expected[i], tol );
+			t.ok( bool, 'within tolerance. x: '+x[i]+'. y: '+y+'. Expected: '+expected[i]+'. Tolerance: '+tol+'.' );
+		}
+	}
+	t.end();
 });
 
 
